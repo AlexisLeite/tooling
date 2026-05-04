7e193d76623903e55c3458a88b5e4fab7b2529d0
Alexis Leite <alexisleite.uy@gmail.com>
2026-05-04 00:41:18 -0300

    add package

--- test/.upm/bin/cm.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-node "C:\Code\tooling\test\.upm\packages\focus.matters__console-monitor@0.1.1\bin\cm.js" %*
--- test/.upm/packages/focus.matters__console-monitor@0.1.1/bin/console-monitor-mcp.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { main } from "../src/mcp.js";
-
-main().catch((error) => {
-  console.error(`[console-monitor-mcp] ${error.stack || error.message}`);
-  process.exit(1);
-});
